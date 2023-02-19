451d3c51a8ba025a57d1243ab4dfb5b4ec2b846e
wonkang2 <wonkang96@gmail.com>
2023-02-19 18:03:58 +0900

    :tada: Init 프로젝트 초기 설정

---
+++ src/main/java/com/commbti/CommbtiApplication.java
@@ -0,0 +1,13 @@
+package com.commbti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommbtiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommbtiApplication.class, args);
+    }
+
+}
