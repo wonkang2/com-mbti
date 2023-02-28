60a2e641a9e39c3af9ec0197b8f55f5b0f902ad3
wonkang2 <wonkang96@gmail.com>
2023-02-28 23:43:25 +0900

    :white_check_mark: [Test] PostsService 테스트 코드 작성 완료

--- src/test/java/com/commbti/domain/board/service/PostsServiceTest.java
+++ src/test/java/com/commbti/domain/board/service/PostsServiceTest.java
@@ -2,9 +2,11 @@ package com.commbti.domain.board.service;
 
 import com.commbti.domain.board.dto.BoardPatchDto;
 import com.commbti.domain.board.dto.BoardPostDto;
+import com.commbti.domain.board.dto.BoardResponseDto;
 import com.commbti.domain.board.entity.Board;
 import com.commbti.domain.board.repository.BoardRepository;
 import com.commbti.domain.file.service.FileService;
+import com.commbti.domain.member.entity.MbtiType;
 import com.commbti.domain.member.entity.Member;
 import com.commbti.domain.member.service.MemberService;
 import org.assertj.core.api.Assertions;
@@ -18,9 +20,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
@@ -215,6 +219,102 @@ public class PostsServiceTest {
             Assertions.assertThatThrownBy(
                             () -> postsService.updateBoard(fakeMemberId, fakeBoardId, fakeBoardPatchDto))
                     .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("해당 게시글이 존재하지 않습니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("게시물 조회 테스트")
+    class FindBoardTest {
+
+        @DisplayName("게시글을 조회하였을 때 제대로 조회되는지 확인")
+        @Test
+        void 게시물_조회_테스트() {
+            Long memberId = 1L;
+            String nickname = "사용자A";
+            Member member = new Member();
+            ReflectionTestUtils.setField(member, "id", memberId);
+            ReflectionTestUtils.setField(member, "nickname", nickname);
+            member.updateMbtiType(MbtiType.INTJ);
+
+            Long boardId = 1L;
+            Board savedBoard = Board.createBoard("테스트제목", "테스트내용", "/test.jpg", member);
+            LocalDateTime createdAt = LocalDateTime.now();
+            ReflectionTestUtils.setField(savedBoard, "createdAt", createdAt);
+
+            given(boardRepository.findById(boardId)).willReturn(Optional.of(savedBoard));
+
+            BoardResponseDto result = postsService.findBoard(boardId);
+
+            assertThat(result.getTitle()).isEqualTo(savedBoard.getTitle());
+            assertThat(result.getContent()).isEqualTo(savedBoard.getContent());
+            assertThat(result.getFilePath()).isEqualTo(savedBoard.getFilePath());
+            assertThat(result.getNickname()).isEqualTo(savedBoard.getMember().getNickname());
+            assertThat(result.getMbtiType()).isEqualTo(savedBoard.getMember().getMbtiType());
+            assertThat(result.getCreatedAt()).isEqualTo(savedBoard.getCreatedAt());
+        }
+
+        @DisplayName("존재하지 않는 게시글을 조회하였을 때 예외가 발생하는지 확인")
+        @Test
+        void 존재하지_않는_게시물_조회_테스트() {
+            Long boardId = 1L;
+
+            given(boardRepository.findById(boardId)).willReturn(Optional.ofNullable(null));
+
+
+            assertThatThrownBy(() -> {
+                postsService.findBoard(boardId);
+            }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("존재하지 않는 게시글입니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("게시물 삭제 테스트")
+    class DeleteBoardTest {
+
+        @Test
+        @DisplayName("게시물이 정상적으로 삭제되는지 확인")
+        void 게시물_삭제_테스트() {
+            Long memberId = 1L;
+            Member member = new Member();
+            ReflectionTestUtils.setField(member, "id", memberId);
+
+            Long boardId = 1L;
+            Board savedBoard = Board.createBoard("테스트제목", "테스트내용", "/test.jpg", member);
+
+            given(boardRepository.findById(boardId)).willReturn(Optional.of(savedBoard));
+            willDoNothing().given(boardRepository).delete(savedBoard);
+
+            boolean result = postsService.deleteBoard(memberId, boardId);
+
+            assertThat(result).isTrue();
+        }
+        @Test
+        @DisplayName("존재하지 않는 게시물을 삭제 요청하였을 때 예외가 발생하는지 확인")
+        void 존재하지_않는_게시물_삭제_테스트() {
+            Long memberId = 1L;
+            Long fakeBoardId = 100L;
+            given(boardRepository.findById(fakeBoardId)).willReturn(Optional.ofNullable(null));
+
+            assertThatThrownBy(() -> {
+                postsService.deleteBoard(memberId, fakeBoardId);
+            }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("존재하지 않는 게시글입니다.");
+        }
+        @Test
+        @DisplayName("권한없는 유저가 삭제 요청하였을 때 예외가 발생하는지 확인")
+        void 권한없는_유저_게시물_삭제_테스트() {
+            Long memberId = 1L;
+            Long fakeMemberId = 100L;
+            Member member = new Member();
+            ReflectionTestUtils.setField(member, "id", memberId);
+
+            Long boardId = 1L;
+            Board savedBoard = Board.createBoard("테스트제목", "테스트내용", "/test.jpg", member);
+
+            given(boardRepository.findById(boardId)).willReturn(Optional.of(savedBoard));
+
+            assertThatThrownBy(() -> {
+                postsService.deleteBoard(fakeMemberId, boardId);
+            }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("잘못된 접근입니다.");
         }
     }
 }
