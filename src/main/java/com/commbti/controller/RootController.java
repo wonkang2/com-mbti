8d061c88ce994d6b3322ca9d975e9f0fe0639f2b
wonkang2 <wonkang96@gmail.com>
2023-03-27 23:18:31 +0900

    [Feat]: root 페이지 변경
    * '/' 루트 페이지를 forward로 게시글 목록 조회 페이지로 변경하였음.

---
+++ src/main/java/com/commbti/controller/RootController.java
@@ -0,0 +1,17 @@
+package com.commbti.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("/")
+@Controller
+public class RootController {
+
+    @GetMapping
+    public String getRootPage() {
+        return "forward:/bulletin-board";
+    }
+}
