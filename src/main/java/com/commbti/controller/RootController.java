f72b32778940a3d67bddc1647a974311901f3306
wonkang2 <wonkang96@gmail.com>
2023-03-28 23:21:47 +0900

    [Refactor]: root 페이지 forward시 쿼리파라미터(페이지) 추가

--- src/main/java/com/commbti/controller/RootController.java
+++ src/main/java/com/commbti/controller/RootController.java
@@ -2,6 +2,7 @@ package com.commbti.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +13,6 @@ public class RootController {
 
     @GetMapping
     public String getRootPage() {
-        return "forward:/bulletin-board";
+        return "forward:/bulletin-board?page=1&size=10";
     }
 }
