807c85f2a3bba58cf6b3720c061cefb98d0a5ae3
wonkang2 <wonkang96@gmail.com>
2023-03-29 23:55:35 +0900

    [Refactor]: 게시글 페이지 조회 수정
    * 현재 사용하지 않는 코드 삭제 및 페이지 구현 시 커밋되지 않았던 데이터를 위해 커밋함.

--- src/main/java/com/commbti/domain/bulletinboard/service/BoardService.java
+++ src/main/java/com/commbti/domain/bulletinboard/service/BoardService.java
@@ -1,18 +1,14 @@
 package com.commbti.domain.bulletinboard.service;
 
-import com.commbti.domain.bulletinboard.dto.BoardResponseDto;
+import com.commbti.domain.bulletinboard.dto.BoardPageDto;
 import com.commbti.domain.bulletinboard.entity.Bulletin;
 import com.commbti.domain.bulletinboard.repository.BulletinBoardRepository;
-import com.commbti.domain.member.entity.MbtiType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -23,31 +19,18 @@ public class BoardService {
     private final BulletinBoardRepository bulletinBoardRepository;
 
     // 게시글 조회(전체)
-    public List<BoardResponseDto> findBoardListPage(int page, int size) {
+    public BoardPageDto findBoardPage(int page, int size) {
         log.trace("findBoardListPage 호출: page={}, size={}", page, size);
         // 기본값: page=0; size= 10;
         PageRequest pageRequest = PageRequest.of(page - 1, size);
         Page<Bulletin> boardPage = bulletinBoardRepository.findPageWithMemberMbti(pageRequest);
 
-        log.debug("boardPage.count = {}", boardPage.stream().count());
-        log.debug("현재 저장되어 있는 bulletin의 개수: {}", bulletinBoardRepository.count());
+        log.debug("Page<Bulletin>: totalPages={}, getTotalElements={}, hasNext={}, hasPrev={}", boardPage.getTotalPages(), boardPage.getTotalElements(), boardPage.hasNext(), boardPage.hasPrevious());
 
-        List<BoardResponseDto> response = boardPage.stream().map(article -> article.toBoardResponseDto())
-                .collect(Collectors.toList());
-        log.debug("response.size = {}", response.size());
+        BoardPageDto response = BoardPageDto.toPageDto(boardPage);
+        log.debug("BoardPageDto.PageInfo: startNumber={}, endNumber={}", response.getPageInfo().getStartNumber(), response.getPageInfo().getEndNumber());
+
         log.trace("findBoardListPage 정상");
         return response;
     }
-
-    // 게시글 조회(mbti별)
-//    public List<BoardResponseDto> findBoardListPageByMbit(int page, int size, MbtiType mbti) {
-//        PageRequest pageRequest = PageRequest.of(page, size);
-//        List<Bulletin> bulletinList = bulletinBoardRepository.findPageByMbti(pageRequest, mbti);
-//
-//        return bulletinList.stream().map(article -> article.toBoardResponseDto()).collect(Collectors.toList());
-//    }
-
-
-
-
 }
