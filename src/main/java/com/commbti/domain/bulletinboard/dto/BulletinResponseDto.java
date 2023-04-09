1a6f73f9b0d06d0fc2fb0836901cb147d50e6bb3
wonkang2 <wonkang96@gmail.com>
2023-04-10 00:05:16 +0900

    [Refactor]: 댓글과 게시물 ResponseDto 필드 추가
    * View에서 내가 쓴 글인지 확인해야 했고, responseDto에 memberId를 추가하여 View에서 현재 로그인한 유저의 memberId와 받은 response에서 memberId를 비교하는 식으로 변경하기 위해 추가함.
    * Comment: toDto 메서드에 변경사항을 적용함.
    * Bulletin: 이전 커밋으로 같이 변경되었음.

--- src/main/java/com/commbti/domain/bulletinboard/dto/BulletinResponseDto.java
+++ src/main/java/com/commbti/domain/bulletinboard/dto/BulletinResponseDto.java
@@ -1,29 +1,33 @@
 package com.commbti.domain.bulletinboard.dto;
 
+import com.commbti.domain.file.dto.ImageFileResponseDto;
 import com.commbti.domain.file.entity.ImageFile;
 import com.commbti.domain.member.entity.MbtiType;
 import com.commbti.global.date.DateUtils;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class BulletinResponseDto {
+    private Long memberId;
     private Long bulletinId;
     private MbtiType mbtiType;
     private String title;
     private String content;
-    private List<String> filePathList;
+    private List<ImageFileResponseDto> imageFileResponseDtoList;
     private Long viewCount;
     private String createdAt;
 
-    private BulletinResponseDto() {
-    }
     @Builder
-    public BulletinResponseDto(Long bulletinId, MbtiType mbtiType, String title, String content, List<ImageFile> imageFileList, Long viewCount, LocalDateTime createdAt) {
+    public BulletinResponseDto(Long memberId, Long bulletinId, MbtiType mbtiType, String title, String content, List<ImageFile> imageFileList, Long viewCount, LocalDateTime createdAt) {
+        this.memberId = memberId;
         this.bulletinId = bulletinId;
         this.mbtiType = mbtiType;
         this.title = title;
@@ -36,9 +40,9 @@ public class BulletinResponseDto {
 
     private void imageFileListToFilePathList(List<ImageFile> imageFileList) {
         if (!imageFileList.isEmpty()) {
-            this.filePathList = imageFileList.stream().map(imageFile -> imageFile.getFilepath()).collect(Collectors.toList());
+            this.imageFileResponseDtoList = imageFileList.stream().map(imageFile -> imageFile.toResponseDto()).collect(Collectors.toList());
         } else {
-            filePathList = null;
+            imageFileResponseDtoList = null;
         }
     }
 }
--- src/main/java/com/commbti/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/commbti/domain/comment/dto/CommentResponseDto.java
@@ -2,21 +2,23 @@ package com.commbti.domain.comment.dto;
 
 import com.commbti.domain.member.entity.MbtiType;
 import com.commbti.global.date.DateUtils;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentResponseDto {
+    private Long memberId;
     private Long commentId;
     private MbtiType mbtiType;
     private String content;
     private String createdAt;
 
-    public CommentResponseDto(Long commentId, MbtiType mbtiType, String content, LocalDateTime createdAt) {
+    public CommentResponseDto(Long memberId, Long commentId, MbtiType mbtiType, String content, LocalDateTime createdAt) {
+        this.memberId = memberId;
         this.commentId = commentId;
         this.mbtiType = mbtiType;
         this.content = content;
