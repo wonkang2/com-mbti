7b4efe093f11302d89f24a2a6901e36a1d39653e
wonkang2 <wonkang96@gmail.com>
2023-04-24 04:55:38 +0900

    [Fix]: 페이지 기능 수정
    * 잘못된 코드로 인해 페이지 기능이 정상적으로 동작하지 않는 오류를 수정하였음.

--- src/main/java/com/commbti/global/page/PageInfo.java
+++ src/main/java/com/commbti/global/page/PageInfo.java
@@ -34,7 +34,9 @@ public class PageInfo {
     }
 
     public void calculateStartPageNumberAndEndPageNumber() {
-        this.endNumber = Math.ceil((currentPage / 10.0) * 10) > totalPage ? (int)(Math.ceil((currentPage / 10.0) * 10)) : totalPage;
+        // 1 2 3 4 5 6 7 8 9 10
+        // 11 12 13 14 15 16 17 18 19 20
+        this.endNumber = Math.ceil(currentPage / 10.0) * 10 < totalPage ? (int)(Math.ceil(currentPage / 10.0) * 10) : totalPage;
         this.startNumber = (endNumber - 9) > 0 ? endNumber - 9 : 1;
     }
 }
