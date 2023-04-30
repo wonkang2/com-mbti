d2c8011aeb91d7c452be4e89b91c0f7355c5b84b
wonkang2 <wonkang96@gmail.com>
2023-04-30 21:35:24 +0900

    [Feat]: ViewExceptionHandler 구현

--- src/main/java/com/commbti/global/exception/ExceptionAdvice.java
+++ src/main/java/com/commbti/global/exception/ExceptionAdvice.java
@@ -3,17 +3,21 @@ package com.commbti.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity handleBusinessLogicException(BusinessLogicException businessLogicException) {
-        ErrorResponse response = ErrorResponse.of(businessLogicException.getExceptionCode());
+    /**
+     * API Exception
+     */
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity handleApiException(ApiException apiException) {
+        ErrorResponse response = ErrorResponse.of(apiException.getExceptionCode());
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
@@ -23,5 +27,18 @@ public class ExceptionAdvice {
                 .get(0)
                 .getDefaultMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+    /**
+     * View Exception
+     */
+    @ExceptionHandler(ViewException.class)
+    public String handleViewException(Model model,
+                                      ViewException viewException) {
+        String imagePath = "/images/error/";
+        ExceptionCode exceptionCode = viewException.getExceptionCode();
+        model.addAttribute("image", imagePath + exceptionCode.getStatus() + ".jpeg");
+        model.addAttribute("message", exceptionCode.getMessage());
+        return "error/error";
     }
 }
